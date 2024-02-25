9be8b252fd601f06c540350dbf24d091623b8f88
jae9380 <ljy5314@gmail.com>
2024-02-25 18:48:06 +0900

    sections-2

---
+++ src/main/java/io/springbarchlecture/springbatch/DBJobConfiguration.java
@@ -0,0 +1,46 @@
+package io.springbarchlecture.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class DBJobConfiguration {
+
+    @Bean
+    public Job job(JobRepository jobRepository, Step step1, Step step2) {
+        return new JobBuilder("Job",jobRepository)
+                .start(step1)
+                .next(step2)
+                .build();
+    }
+
+    @Bean
+    public Step step1(JobRepository jobRepository, PlatformTransactionManager ptm) {
+        return new StepBuilder("step1",jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step1 was executed");
+                    return RepeatStatus.FINISHED;
+                },ptm)
+                .build();
+    }
+
+    @Bean
+    public Step step2(JobRepository jobRepository, PlatformTransactionManager ptm) {
+        return new StepBuilder("step2",jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step2 was executed");
+                    return RepeatStatus.FINISHED;
+                },ptm)
+                .build();
+    }
+
+}
