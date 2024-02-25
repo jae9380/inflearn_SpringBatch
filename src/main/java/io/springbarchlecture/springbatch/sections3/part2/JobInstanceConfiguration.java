2435928bd7c1f882a8a7ae7717fec77e15474d5f
jae9380 <ljy5314@gmail.com>
2024-02-26 01:48:56 +0900

    sections-3/ part-3

--- src/main/java/io/springbarchlecture/springbatch/sections3/part2/JobInstanceConfiguration.java
+++ src/main/java/io/springbarchlecture/springbatch/sections3/part2/JobInstanceConfiguration.java
@@ -1,59 +1,59 @@
-package io.springbarchlecture.springbatch.sections3.part2;
-
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.PlatformTransactionManager;
-
-@Configuration
-public class JobInstanceConfiguration {
-
-//    지금 구성한 Job과 JobParameters의 값이 동일 할 경우와 동일하지 않을 경우 테스트
-
-    @Bean
-    public Job job(JobRepository jobRepository, Step step1, Step step2) {
-        return new JobBuilder("job",jobRepository)
-                .start(step1)
-                .next(step2)
-                .build();
-    }
-
-    @Bean
-    public Step step1(JobRepository jobRepository, PlatformTransactionManager ptm) {
-        return new StepBuilder("ste1",jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step1 was executed");
-                    return RepeatStatus.FINISHED;
-                },ptm)
-                .build();
-    }
-
-    @Bean
-    public Step step2(JobRepository jobRepository, PlatformTransactionManager ptm) {
-        return new StepBuilder("ste2",jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step2 was executed");
-                    return RepeatStatus.FINISHED;
-                },ptm)
-                .build();
-    }
-
-//    최초 실핼 시
-//    batch_job_instance, batch_job_execution_params 테이블에 해당 정보가 잘 저장이 되었다.
-
-//    동일한 내용으로 재 실핼 시
-//     A job instance already exists and
-//     is complete for identifying parameters={'name':'{value=user1, type=class java.lang.String, identifying=true}'}.
-//     If you want to run this job again, change the parameters.
-//    위 내용처럼 한 줄로 오류에 대한 메세지와 함께 오류를 발생 시킨다.
-
-//    기존 user1 -> user2로 변경하여 실행
-//    batch_job_instance 테이블 조회 시 Job_Key 값이 다른 2개의 데이터가 저장
-//    batch_job_execution_params 테이블에는 user2에 대한 데이터 저장
-
-}
+//package io.springbarchlecture.springbatch.sections3.part2;
+//
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.job.builder.JobBuilder;
+//import org.springframework.batch.core.repository.JobRepository;
+//import org.springframework.batch.core.step.builder.StepBuilder;
+//import org.springframework.batch.repeat.RepeatStatus;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.transaction.PlatformTransactionManager;
+//
+//@Configuration
+//public class JobInstanceConfiguration {
+//
+////    지금 구성한 Job과 JobParameters의 값이 동일 할 경우와 동일하지 않을 경우 테스트
+//
+//    @Bean
+//    public Job job(JobRepository jobRepository, Step step1, Step step2) {
+//        return new JobBuilder("job",jobRepository)
+//                .start(step1)
+//                .next(step2)
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step1(JobRepository jobRepository, PlatformTransactionManager ptm) {
+//        return new StepBuilder("ste1",jobRepository)
+//                .tasklet((contribution, chunkContext) -> {
+//                    System.out.println("step1 was executed");
+//                    return RepeatStatus.FINISHED;
+//                },ptm)
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step2(JobRepository jobRepository, PlatformTransactionManager ptm) {
+//        return new StepBuilder("ste2",jobRepository)
+//                .tasklet((contribution, chunkContext) -> {
+//                    System.out.println("step2 was executed");
+//                    return RepeatStatus.FINISHED;
+//                },ptm)
+//                .build();
+//    }
+//
+////    최초 실핼 시
+////    batch_job_instance, batch_job_execution_params 테이블에 해당 정보가 잘 저장이 되었다.
+//
+////    동일한 내용으로 재 실핼 시
+////     A job instance already exists and
+////     is complete for identifying parameters={'name':'{value=user1, type=class java.lang.String, identifying=true}'}.
+////     If you want to run this job again, change the parameters.
+////    위 내용처럼 한 줄로 오류에 대한 메세지와 함께 오류를 발생 시킨다.
+//
+////    기존 user1 -> user2로 변경하여 실행
+////    batch_job_instance 테이블 조회 시 Job_Key 값이 다른 2개의 데이터가 저장
+////    batch_job_execution_params 테이블에는 user2에 대한 데이터 저장
+//
+//}
--- src/main/java/io/springbarchlecture/springbatch/sections3/part2/JobRunner.java
+++ src/main/java/io/springbarchlecture/springbatch/sections3/part2/JobRunner.java
@@ -1,30 +1,30 @@
-package io.springbarchlecture.springbatch.sections3.part2;
-
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
-
-@Component
-public class JobRunner implements ApplicationRunner {
-
-    @Autowired
-    private JobLauncher jobLauncher;
-    @Autowired
-    private Job job;
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-
-        JobParameters jobParameters = new JobParametersBuilder()
-                .addString("name", "user2")
-//                JobParametersBuilder 에서 Date, String, Double, Long 총 4가지 타입을 지정
-                .toJobParameters();
-
-        jobLauncher.run(job,jobParameters);
-    }
-}
+//package io.springbarchlecture.springbatch.sections3.part2;
+//
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.JobParameters;
+//import org.springframework.batch.core.JobParametersBuilder;
+//import org.springframework.batch.core.launch.JobLauncher;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.ApplicationArguments;
+//import org.springframework.boot.ApplicationRunner;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class JobRunner implements ApplicationRunner {
+//
+//    @Autowired
+//    private JobLauncher jobLauncher;
+//    @Autowired
+//    private Job job;
+//
+//    @Override
+//    public void run(ApplicationArguments args) throws Exception {
+//
+//        JobParameters jobParameters = new JobParametersBuilder()
+//                .addString("name", "user2")
+////                JobParametersBuilder 에서 Date, String, Double, Long 총 4가지 타입을 지정
+//                .toJobParameters();
+//
+//        jobLauncher.run(job,jobParameters);
+//    }
+//}
