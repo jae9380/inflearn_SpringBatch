11ab343f3eaaec1fcc88a5d2a32cd4177ab570d6
jae9380 <ljy5314@gmail.com>
2024-03-08 00:11:21 +0900

    section-4/ part-2/ SimpleJobArchitecture

--- src/main/java/io/springbarchlecture/springbatch/section4/part2/chap1/incrementer/CustomJobParametersIncrementer.java
+++ src/main/java/io/springbarchlecture/springbatch/section4/part2/chap1/incrementer/CustomJobParametersIncrementer.java
@@ -1,21 +1,21 @@
-package io.springbarchlecture.springbatch.section4.part2.chap1.incrementer;
-
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersIncrementer;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-public class CustomJobParametersIncrementer implements JobParametersIncrementer {
-
-    static final SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd-hhmmss");
-
-    @Override
-    public JobParameters getNext(JobParameters parameters) {
-
-        String id = format.format(new Date());
-
-        return new JobParametersBuilder().addString("run.id", id).toJobParameters();
-    }
-}
+//package io.springbarchlecture.springbatch.section4.part2.chap1.incrementer;
+//
+//import org.springframework.batch.core.JobParameters;
+//import org.springframework.batch.core.JobParametersBuilder;
+//import org.springframework.batch.core.JobParametersIncrementer;
+//
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+//
+//public class CustomJobParametersIncrementer implements JobParametersIncrementer {
+//
+//    static final SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd-hhmmss");
+//
+//    @Override
+//    public JobParameters getNext(JobParameters parameters) {
+//
+//        String id = format.format(new Date());
+//
+//        return new JobParametersBuilder().addString("run.id", id).toJobParameters();
+//    }
+//}
